f36ddeed220c4846f2666368abd6083184670ea1
Luis Lalo <luis.hdz9720@gmail.com>
2020-07-04 17:15:29 -0500

    firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,34 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
+
 
   constructor(private http: HttpClient) {
-    console.log('servicio página')
 
-//leer archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+
+
+  }
+
+  private cargarInfo(){
+    //leer archivo JSON
 
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina)=>{
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-    })
 
+    });
   }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-portafolio-11590.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
