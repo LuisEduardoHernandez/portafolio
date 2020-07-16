e69dff72939456caa8878443308a7dce8acc6f69
Luis Lalo <luis.hdz9720@gmail.com>
2020-07-15 19:31:13 -0500

    casi

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
+
 
 @Component({
   selector: 'app-item',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  nombre = 'Luis Eduardo Hernandez Granillo';
+  correo = 'luis.hdz9720@gmail.com'
+
+  producto: ProductoDescripcion;
+  id_productos: string;
+
+  constructor(private route: ActivatedRoute, public productosService: ProductosService) { }
+
+  ngOnInit() {
+
+    this.route.params
+    .subscribe( parametros => {
+      // console.log(parametros['id']);
+      this.productosService.getProducto(parametros['id'])
+            .subscribe( (producto: ProductoDescripcion) => {
+              this.id_productos = parametros['id'];
+              this.producto = producto;
+
+
+            });
+    });
+
+
   }
 
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscriber } from 'rxjs';
+import { Producto } from '../interfaces/producto.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: Producto[] = [];
+
+
+  constructor(private http: HttpClient) {
+
+    this.cargarProductos();
+  }
+
+
+  private cargarProductos(){
+    this.http.get('https://angular-portafolio-11590.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Producto[]) => {
+      this.productos = resp;
+      this.cargando = false;
+
+    });
+
+  }
+
+  getProducto( id: string ) {
+
+    return this.http.get(`https://angular-portafolio-11590.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+}
